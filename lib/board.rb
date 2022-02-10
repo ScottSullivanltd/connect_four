bb79212c24cb47f5b56501158c04c1625e6fd3b9
Cory Bethune <corybbethune@gmail.com>
2022-02-09 21:14:47 -0700

    Refactor

--- lib/board.rb
+++ lib/board.rb
@@ -1,29 +1,25 @@
 require 'pry'
 
 class Board
-attr_reader
+attr_reader :board_array
 
   def initialize
     @board_array = []
+    @board_array[0] = [" A", " B", " C", " D", " E", " F", " G"]
+    @board_array[1] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
+    @board_array[2] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
+    @board_array[3] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
+    @board_array[4] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
+    @board_array[5] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
+    @board_array[6] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
   end
 
-end
-
-test_array = Array.new(7)
-test_array[0] = [" A", " B", " C", " D", " E", " F", " G"]
-test_array[1] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
-test_array[2] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
-test_array[3] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
-test_array[4] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
-test_array[5] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
-test_array[6] = [" ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", ".", " ", "."]
-# puts test_array[0][0 .. 6]
-# p test_array[0][0 .. 6]
-test_array.each do |row|
-  row.each do |col|
-    print col
+  def print_board
+    test_array.each do |row|
+      row.each do |col|
+        print col
+      end
+      puts
+    end
   end
-  puts
 end
-
-binding pry
