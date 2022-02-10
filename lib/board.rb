cd5c96a3bb3ed0eb2df9c2779a2385e8d14aacb9
Scott Sullivan <scottsullivanltd@gmail.com>
2022-02-10 11:53:53 -0700

    feat: have working blank board

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,20 @@
 class Board
+  attr_reader :new_board
+  def initialize
+    @new_board = new_board
+  end
+
+  new_board = [
+    [" A", "B", "C", "D", "E", "F", "G"],
+    [" .",".",".",".",".",".","."],
+    [" .",".",".",".",".",".","."],
+    [" .",".",".",".",".",".","."],
+    [" .",".",".",".",".",".","."],
+    [" .",".",".",".",".",".","."],
+    [" .",".",".",".",".",".","."]
+  ]
+
+  new_board.each do |row|
+    puts row.join(' ')
+  end
 end
