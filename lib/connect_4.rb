e881cfc0fbaee4a7018a62bb0280a28d5df741f5
Cory Bethune <corybbethune@gmail.com>
2022-02-09 18:48:23 -0700

    Initial commit

---
+++ lib/connect_4.rb
@@ -0,0 +1,9 @@
+p "Welcome to Cory and Scotts' Connect 4 - Virtual Edition!"
+p "The rules are simple. Connect 4 of your 'X' pieces in a row to win."
+p "A winning row can be horizontal, vertical, or diagonal."
+p "The player moves first, then the computer."
+p "What is your name?"
+
+name = gets.chomp
+
+#menu method here maybe
