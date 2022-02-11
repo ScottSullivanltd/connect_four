95c37e326b76257dbfc69f85ed414c40add5b081
Cory Bethune <corybbethune@gmail.com>
2022-02-10 20:57:39 -0700

    Refactor

--- lib/player.rb
+++ lib/player.rb
@@ -1,26 +1,31 @@
 require 'pry'
 
 class Player
-  attr_reader :name, :pieces, :c_name, :c_pieces
+  attr_reader :name, :pieces
 
-  def initialize(name)
+  def initialize(name = "Computer Overlord")
     @name = name
     @pieces = 21
-    @c_name = "Computer Overlord"
-    @c_pieces = 21
   end
 
   def player_column_choice #gets the user input and returns the input as a cap letter
-    input = ""            #outer if loop to check if player is the computer or not needed here
-    until input == "A" || "B" || "C" || "D" || "E" || "F" || "G" do
-      puts "Which column would you like to drop a piece into?"
-      puts "Please input the letter at the top of the column you want to pick."
-      input = gets.chomp.capitalize
+    if :name == "Computer Overlord"
+      
 
-      if input == "A" || "B" || "C" || "D" || "E" || "F" || "G"
-        return input
-      else
-        puts "That is not a valid response. Please input a letter at the top of a column."
+
+
+    else
+      input = ""            #outer if loop to check if player is the computer or not needed here
+      while input == "" || input == "A" || input == "B" || input == "C" || input == "D" || input == "E" || input == "F" || input == "G" do
+        puts "Which column would you like to drop a piece into?"
+        puts "Please input the letter at the top of the column you want to pick."
+        input = gets.chomp.capitalize
+
+        if input == "A" || input == "B" || input == "C" || input == "D" || input == "E" || input == "F" || input == "G"
+          return input #this does return the input
+        else
+          puts "That is not a valid response. Please input a letter from the top of a column."
+        end
       end
     end
   end
