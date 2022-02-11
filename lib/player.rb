84199604c11fe43c2b6d8f43123c2b88519a5ce4
Cory Bethune <corybbethune@gmail.com>
2022-02-10 18:37:36 -0700

    Refactor: rough draft final form

--- lib/player.rb
+++ lib/player.rb
@@ -10,11 +10,19 @@ class Player
     @c_pieces = 21
   end
 
-  def place_piece
-    puts "Which column would you like to drop a piece into?"
-    puts "Please input the letter at the top of the column you want to pick."
+  def place_piece #gets the user input and returns the input as a cap letter
+    input = ""
+    until input == "A" || "B" || "C" || "D" || "E" || "F" || "G" do
+      puts "Which column would you like to drop a piece into?"
+      puts "Please input the letter at the top of the column you want to pick."
+      input = gets.chomp.capitalize
 
-    return gets.chomp
+      if input == "A" || "B" || "C" || "D" || "E" || "F" || "G"
+        return input
+      else
+        puts "That is not a valid response. Please input a letter at the top of a column."
+      end
+    end
   end
 
 end
