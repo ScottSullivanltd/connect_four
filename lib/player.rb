1430da7e77f114cbf8c16225eb206105bfd1d7df
Cory Bethune <corybbethune@gmail.com>
2022-02-11 11:24:17 -0700

    Refactor

--- lib/player.rb
+++ lib/player.rb
@@ -9,13 +9,13 @@ class Player
     @array = ["A", "B", "C", "D", "E", "F", "G"]
   end
 
-  def player_column_choice #gets the user/computer input and returns the input as a cap letter 
+  def player_column_choice #gets the user/computer input and returns the input as a cap letter
     if @name == "Computer Overlord"
       c_input = @array.sample
       return c_input #this works!
     else
       input = ""
-      while input == "" || input == "A" || input == "B" || input == "C" || input == "D" || input == "E" || input == "F" || input == "G" do
+      while input == "" do
         puts "Which column would you like to drop a piece into?"
         puts "Please input the letter at the top of the column you want to pick."
         input = gets.chomp.capitalize
@@ -25,6 +25,7 @@ class Player
         else
           puts "That is not a valid response. Please input a letter from the top of a column."
         end
+        input = ""
       end
     end
   end
